4c741748a18f957a94595ec387b75898cd298a68
Boris Vergasov <starsfer839@yandex.ru>
2026-06-16 15:11:18 +0300

    настройка и обновление версии тестов и имя базы для тестов миграции в Postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -32,7 +32,7 @@ import { Ticket } from './order/entities/ticket.entity';
         port: configService.get<number>('DATABASE_PORT', 5432),
         username: configService.get<string>('DATABASE_USERNAME', 'prac'),
         password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME', 'prac'),
+        database: configService.get<string>('DATABASE_NAME') || 'films',
         entities: [Film, Schedule, Order, Ticket],
         synchronize: false, // таблицы созданы вручную
         logging: true, // для отладки
